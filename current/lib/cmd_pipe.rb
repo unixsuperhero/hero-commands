1c34bc97829564f063890bad3c7aeb1de0e593cc
Joshua Toyota <jearsh@gmail.com>
2016-06-11 11:24:43 -0400

    improved the class that handles piping shell commands together

--- current/lib/cmd_pipe.rb
+++ current/lib/cmd_pipe.rb
@@ -1,114 +1,93 @@
 
 class CmdPipe
   attr_accessor :commands, :pipes, :options
+  attr_accessor :reader, :writer
   def initialize(*args)
     @options = args.last.is_a?(Hash) ? args.pop : {}
-    @commands = args.flatten.map{|cmd| Command.new(cmd) }
-    @pipes = Pipe.from_commands(@commands)
+    @commands = CommandChain.from_list(args.flatten) #.map{|cmd| Command.new(cmd) }
+    @reader, @writer = IO.pipe
+    # @pipes = Pipe.from_commands(@commands)
   end
 
-  def start
-    @commands.reverse.each(&:start)
+  def run
+    last_command = commands.last_command
+    last_command.writer = writer
+
+    commands.run
+
+    reader.read.tap{|r|
+      r.close
+      writer.close unless writer.closed?
+    }
   end
 
-  def statuses
-    @commands.map(&:status)
+  def output
+    @output ||= run
   end
 
-  def stop
-    @commands.each(&:stop)
-    @commands.each(&:kill)
+  def print
+    puts output
   end
 
-  class Pipe
+  class CommandChain
     class << self
-      def all
-        @all_pipes ||= []
+      attr_accessor :first_command, :last_command
+
+      def set_first_command(cmd)
+        @first_command = cmd
       end
 
-      def from_commands(*commands)
-        (commands.flatten.count - 1).
-          times.
-          flat_map{ IO.pipe }.
-          push(nil).
-          reverse.
-          push(nil).
-          each_slice(2).
-          map{|rw| new(*rw) }.
-          tap{|new_pipes|
-            commands.flatten.zip(new_pipes).each{|cmd,pipe|
-              cmd.pipe = pipe
-              pipe.command = cmd
-            }
-          }
+      def set_last_command(cmd)
+        @last_command = cmd
+      end
+
+      def from_list(cmds)
+        cmds.reverse.inject(nil){|piping_to,shell_cmd|
+          new(shell_cmd, piping_to).tap do |precmd|
+            if piping_to
+              piping_to.precmd = precmd
+            else
+              set_last_command(precmd)
+            end
+          end
+        }.tap{|first_command|
+          set_first_command(first_command)
+          cmd = first_command
+          r,w = nil,nil
+          while cmd.subcmd
+            r,w = IO.pipe
+            cmd.writer = w
+            cmd.subcmd.reader = r
+            cmd = cmd.subcmd
+          end
+        }
       end
     end
 
-    attr_reader :reader, :writer
-    attr_reader :input, :output
-    attr_reader :closed
-    attr_accessor :command
-    def initialize(r=nil, w=nil)
-      @reader, @writer = r, w
-      @input, @output = r, w
-      @closed = false
-      self.class.all.push self
+    attr_accessor :shell_cmd, :subcmd, :precmd
+    attr_accessor :reader, :writer
+    def initialize(shell_cmd, to_cmd=nil, from_cmd=nil)
+      @shell_cmd, @subcmd, @precmd = shell_cmd, to_cmd, from_cmd
     end
 
-    def close
-      return if closed
-      writer.close if writer
+    def last_command
+      subcmd ? subcmd.last_command : self
+    end
+
+    def exec
+      opts = {}
+      opts.merge!(in: reader) if reader
+      opts.merge!(out: writer) if writer
+      system(shell_cmd, opts)
+    end
+
+    def run
+      exec
       reader.close if reader
-      @closed = true
-    end
-  end
-
-  class Command
-    class << self
-      def all
-        @all_commands ||= []
+      writer.close if writer
+      if subcmd
+        subcmd.run
       end
-    end
-
-    attr_accessor :cmd, :options
-    attr_accessor :pid, :active
-    attr_accessor :pipe
-
-    def initialize(cmd, options={})
-      @cmd, @options = cmd, options
-      self.class.all.push self
-    end
-
-    def build_options
-      @options.tap do |opts|
-        opts.merge! in: @pipe.reader if @pipe.reader
-        opts.merge! out: @pipe.writer if @pipe.writer
-      end
-    end
-
-    def start
-      return if @active == false
-      @pid = spawn(cmd, build_options)
-      @active = true
-      @killed = false
-    end
-
-    def status
-      #Process.wait2(pid)
-    end
-
-    def stop
-      return unless @active == true
-      pipe.close
-      @active = false
-    end
-
-    def kill
-      return
-      return if @killed == true
-      # Process.detach(pid)
-      Process.kill(:KILL, pid)
-      @killed = true
     end
   end
 end
