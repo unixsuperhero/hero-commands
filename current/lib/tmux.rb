91f1b29099d3811828dfde94e4c18e83e3213104
Joshua Toyota <jearsh@gmail.com>
2016-06-15 21:27:00 -0400

    bugfix for SubcommandMatcher

--- current/lib/tmux.rb
+++ current/lib/tmux.rb
@@ -1,5 +1,16 @@
 class Tmux
   class << self
+    def session_exists?(name)
+      `tmux ls`.lines.any?{|l| l[/^#{name}:/] }
+    end
+
+    def attach_to_session(name)
+      exec("tmux a -t #{name}")
+    end
+
+    def new_session(name, path=Dir.pwd, *args)
+      exec("tmux new -s #{name} -c #{path} " + args.join(' '))
+    end
   end
 end
 
