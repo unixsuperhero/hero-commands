fec09b9c52ffa59bbf47d6ea1073ce004dbd44ff
Joshua Toyota <jearsh@gmail.com>
2016-06-19 16:47:48 -0400

    committing current/ after rebuilding it

--- current/lib/subcommandable.rb
+++ current/lib/subcommandable.rb
@@ -145,7 +145,7 @@ module ShellCommandable
     base.extend(ClassMethods)
 
     def base.clet(name,&blk)
-      base.define_method(name, &blk)
+      self.define_method(name, &blk)
     end
   end
 
