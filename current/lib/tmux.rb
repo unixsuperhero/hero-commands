a10aca33437f690e7c4306d4dbd431e33d15df23
Joshua Toyota <jearsh@gmail.com>
2016-06-23 01:30:29 -0400

    current/: add new tmux cmd to switch current client to a session if TMUX env var already exists

--- current/lib/tmux.rb
+++ current/lib/tmux.rb
@@ -8,12 +8,28 @@ class Tmux
       sessions.include?(name)
     end
 
+    def switch_session(name)
+      exec("tmux switchc -t #{name}")
+    end
+
     def attach_to_session(name)
       exec("tmux a -t #{name}")
     end
 
     def new_session(name, path=Dir.pwd, *args)
       exec("tmux new -s #{name} -c #{path} " + args.join(' '))
+    end
+
+    def force_session(name, path=Dir.pwd, *args)
+      if session_exists?(name)
+        if ENV.has_key?('TMUX')
+          switch_session(name)
+        else
+          attach_to_session(name)
+        end
+      else
+        new_session(name, path)
+      end
     end
   end
 end
