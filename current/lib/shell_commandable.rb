e61ee07b4a4ae296f39192fcc169e90a117a462a
Joshua Toyota <jearsh@gmail.com>
2016-06-26 14:43:27 -0400

    current/: implemented command modifiers

--- current/lib/shell_commandable.rb
+++ current/lib/shell_commandable.rb
@@ -11,6 +11,114 @@ module ShellCommandable
       define_singleton_method(name, &block)
     end
 
+    def modifier_args
+      {
+        '@vim' => Proc.new{|returned|
+          HeroHelper.edit_in_editor *returned.flatten
+        },
+        '@capture' => Proc.new{|returned|
+          tempfile = Tempfile.create('hero')
+
+          case returned
+          when String
+            IO.write(tempfile.path, returned)
+            HeroHelper.edit_in_editor(tempfile.path)
+          when Array
+            IO.write(tempfile.path, returned.join("\n"))
+            HeroHelper.edit_in_editor(tempfile.path)
+          else
+            puts format('Not sure how to capture a %s...', returned.class)
+          end
+
+          tempfile.delete
+          tempfile.close
+        }
+      }
+    end
+
+    def run(args=ARGV.clone)
+      @args = args
+      @subcommand = args.shift
+
+      modified_with = args.reverse.take_while{|arg|
+        modifier_args.keys.include?(arg)
+      }
+      modified_with.length.times{ args.pop }
+
+      if @subcommand.nil?
+        if @no_subcommand.is_a?(Proc)
+          run_with_hooks{
+            returned = @no_subcommand.call
+            if modified_with.any?
+              modified_with.each do |arg|
+                modifier_args[arg].call(returned)
+              end
+            end
+          }
+          exit 0
+        else
+          print_subcommand_list
+          exit 1
+        end
+      end
+
+      if @subcommand.index(?:) && @subcommand.index(?:) > 0
+        @subcommand, @subcommand_chain = @subcommand.split(?:, 2)
+      end
+
+      @runner = subcommand_matcher.match(@subcommand)
+
+      if @runner
+        command_output = nil
+        run_with_hooks{
+          command_output = @runner.data.call
+          if modified_with.any?
+            modified_with.each do |arg|
+              modifier_args[arg].call(command_output)
+            end
+          end
+        }
+
+        # TODO: figure out how to pass specific args to a subcmd in the middle
+        #       of the chain...we could do something like subcmdname:value
+        #       so for example: h git branch:checkout feature.date.autoparser checkout:master
+        #
+        #       maybe instead of using just the subcmd arg to specify what is
+        #       chained in the same handler, use a separator that sits between
+        #       where one set of args end and next subcmd begins like '\;'
+        #       because it is escaped...it shouldn't interfere with bash or zsh
+        #       (and find uses it)
+        if @subcommand_chain
+          if command_output
+            if command_output.is_a?(Array)
+              run([@subcommand_chain] + command_output)
+            else
+              run([@subcommand_chain, command_output])
+            end
+          else
+            run([@subcommand_chain])
+          end
+        end
+
+        exit 0
+      end
+
+      if @dynamic_subcommand
+        run_with_hooks{
+          returned = @dynamic_subcommand.call
+          if modified_with.any?
+            modified_with.each do |arg|
+              modifier_args[arg].call(returned)
+            end
+          end
+        }
+        exit 0
+      end
+
+      puts format('Runner/handler not found for the "%s" subcommand', subcommand)
+      exit 1
+    end
+
     def error_exit(msg=nil, &block)
       puts format('ERROR: %s', msg) if msg
 
@@ -45,63 +153,6 @@ module ShellCommandable
         name,dir = possible_projects.max_by{|name,dir| dir.length }
         ProjectHelper.project_for(name)
       end
-    end
-
-    def run(args=ARGV.clone)
-      @args = args
-      @subcommand = args.shift
-
-      if @subcommand.nil?
-        if @no_subcommand.is_a?(Proc)
-          run_with_hooks{ @no_subcommand.call }
-          exit 0
-        else
-          print_subcommand_list
-          exit 1
-        end
-      end
-
-      if @subcommand.index(?:) && @subcommand.index(?:) > 0
-        @subcommand, @subcommand_chain = @subcommand.split(?:, 2)
-      end
-
-      @runner = subcommand_matcher.match(@subcommand)
-
-      if @runner
-        command_output = nil
-        run_with_hooks{ command_output = @runner.data.call }
-
-        # TODO: figure out how to pass specific args to a subcmd in the middle
-        #       of the chain...we could do something like subcmdname:value
-        #       so for example: h git branch:checkout feature.date.autoparser checkout:master
-        #
-        #       maybe instead of using just the subcmd arg to specify what is
-        #       chained in the same handler, use a separator that sits between
-        #       where one set of args end and next subcmd begins like '\;'
-        #       because it is escaped...it shouldn't interfere with bash or zsh
-        #       (and find uses it)
-        if @subcommand_chain
-          if command_output
-            if command_output.is_a?(Array)
-              run([@subcommand_chain] + command_output)
-            else
-              run([@subcommand_chain, command_output])
-            end
-          else
-            run([@subcommand_chain])
-          end
-        end
-
-        exit 0
-      end
-
-      if @dynamic_subcommand
-        run_with_hooks{ @dynamic_subcommand.call }
-        exit 0
-      end
-
-      puts format('Runner/handler not found for the "%s" subcommand', subcommand)
-      exit 1
     end
 
     def args
