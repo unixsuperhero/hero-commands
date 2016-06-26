dc719b0e59d204a62dc1a49bad3bded0e1ebdd31
Joshua Toyota <jearsh@gmail.com>
2016-06-26 14:09:13 -0400

    current/: simplify Value#initialize...also update @to_h when using a setter method

--- current/lib/name_matcher.rb
+++ current/lib/name_matcher.rb
@@ -3,15 +3,8 @@ class Value
   attr_reader :to_h
 
   def initialize(hash)
-    @to_h = hash
-    hash.each{|k,v|
-      define_singleton_method(k){ v }
-
-      set_method = k.to_s + ?=
-      define_singleton_method(set_method){|new_v|
-        define_singleton_method(k){ new_v }
-      }
-    }
+    @to_h = {}
+    merge(hash)
   end
 
   def merge(hash)
@@ -22,6 +15,7 @@ class Value
 
         set_method = k.to_s + ?=
         define_singleton_method(set_method){|new_v|
+          @to_h.merge!(k => new_v)
           define_singleton_method(k){ new_v }
         }
       }
