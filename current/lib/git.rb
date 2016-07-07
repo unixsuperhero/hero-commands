8f9b024d4749874268fb1693a5d61a98b0183c1d
Joshua Toyota <jearsh@gmail.com>
2016-07-07 01:03:44 -0400

    current/: add ignore subcommand for h-git

--- current/lib/git.rb
+++ current/lib/git.rb
@@ -1,5 +1,57 @@
 class Git
   class << self
+    def inside_repo?
+      system(*'git rev-parse --show-cdup'.shellsplit, [:out, :err] => '/dev/null')
+      # return true if `git rev-parse --is-inside-work-tree`[/true/]
+      # return true if `git rev-parse --is-inside-git-dir`[/true/]
+      # false
+    end
+
+    def path_to_root
+      return '.' unless inside_repo?
+      `git rev-parse --show-cdup`.lines.first.chomp.sub(%r{/*$}, '')
+    end
+
+    def absolute_root_path
+      return '.' unless inside_repo?
+      `git rev-parse --show-toplevel`.lines.first.chomp.sub(%r{/*$}, '')
+    end
+
+    def cd_to_root_path
+      return false unless inside_repo?
+      Dir.chdir(path_to_root)
+    end
+
+    def path_to_git_dir
+      return '.' unless inside_repo?
+      `git rev-parse --git-dir`.lines.first.chomp.sub(%r{/*$}, '')
+    end
+
+    def cd_to_git_dir
+      return false unless inside_repo?
+      Dir.chdir(path_to_git_dir)
+    end
+
+    def current_head
+      return false  unless inside_repo?
+      head = `git symbolic-ref HEAD`.lines.first.chomp
+      head.sub(%r{^refs/heads/}, '')
+    end
+
+    def heads_dir
+      File.join(path_to_git_dir, 'refs/heads')
+    end
+
+    def heads
+      pwd = Dir.pwd
+      Dir.chdir heads_dir
+
+      Dir['**/*'].select{|f|
+        File.file?(f)
+      }.tap{
+        Dir.chdir(pwd)
+      }
+    end
     def current_branch
       `git rev-parse --symbolic-full-name HEAD`.chomp.split(?/,3).last
     end
