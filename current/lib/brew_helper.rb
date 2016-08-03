28b3e7b2758ebfeded208748f4cf91ea3ba9254d
Joshua Toyota <jearsh@gmail.com>
2016-08-02 21:23:19 -0400

    current/: setup h-brew to use Helpers.brew and Helpers.brew_installer for subcommands

--- current/lib/brew_helper.rb
+++ current/lib/brew_helper.rb
@@ -10,8 +10,12 @@ end
 class BrewHelper
 end
 
+class BrewInstaller
+end
+
 class Helpers
   register_helper :brew, BrewHelper
+  register_helper :brew_installer, BrewInstaller
 end
 
 class BrewHelper
@@ -30,24 +34,54 @@ class BrewHelper
       `brew list`.lines.map(&:strip).join(' ').split(/\s+/)
     end
 
+    def installed?(keg)
+      installed.include?(keg)
+    end
+
     def pending
       list - installed
     end
+
+    def install(keg)
+      Helpers.brew_installer.run(keg)
+    end
+  end
+end
+
+class BrewInstaller
+  class << self
+    def run(keg)
+      new(keg).tap(&:run)
+    end
+    alias_method :keg, :run
   end
 
-  class Installer
-    class << self
-      def run(keg)
-        new(keg).tap(&:run)
-      end
-      alias_method :keg, :run
+  attr_reader :keg
+  def initialize(keg)
+    @keg = keg
+  end
+
+  def run
+    if installed?
+      @reason = :installed
+      return @success = false
     end
 
-    attr_reader :keg
-    def initialize(keg)
-      @keg = keg
+    unless @success = system('brew', 'install', keg)
+      @reason = :unknown
     end
+  end
 
+  def installed?
+    Helpers.brew.installed? keg
+  end
+
+  def successful?
+    @success == true
+  end
+
+  def reason
+    @reason
   end
 end
 
